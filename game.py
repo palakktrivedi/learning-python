baa484ed07eba8c1d2e4674da846619e10e71b58
palakktrivedi <ptrivedi2017@gmail.com>
2026-04-05 12:08:04 -0400

    added some rooms and directions next

--- game.py
+++ game.py
@@ -1,3 +1,53 @@
+# === Game Data ===
+
+rooms = {
+     "lab": {
+          "description": "You are in a mysterious lab. There is a door to the north.",
+          "items": ["key"],
+          "exits": {"north": "hallway"}
+     },
+
+     "hallway": {
+          "description": "A long hallway. There is a locked door to the east.",
+          "items": [],
+          "exists": {"south": "lab", "east": "treasure"}
+     },
+
+     "treasure": {
+          "description": "A room filled with treasure! You win!",
+          "items": [],
+          "exists": {}
+     }
+}
+
+inventory = []
+current_room = "lab"
+
+# === Functions ===
+def show_status():
+     print("\n====================")
+     print(f"You are in the {current_room}.")
+     print(rooms[current_room]["description"])
+     print(f"Inventory: {inventory}")
+
+     if rooms[current_room]["items"]:
+          print(f"You see: {rooms[current_room]['items']}")
+
+def get_command():
+     return input("\nWhat do you want to do? ").lower()
+
+
+    
+
+
+
+
+
+
+
+
+
+
 def show_intro():
     print("=== Mystery Lab Adventure ===")
     print("You wake up in a mysterious laboratory.\n")
